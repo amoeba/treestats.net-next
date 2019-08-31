39affb5a2ead54665e508354f4da7ca6f2f8cb78
Bryce Mecum <petridish@gmail.com>
2019-08-31 10:50:13 -0800

    Add base skill levels

--- db/migrations/002_create_skills.rb
+++ db/migrations/002_create_skills.rb
@@ -3,8 +3,10 @@ Sequel.migration do
     create_table(:skills) do
       primary_key :id
       foreign_key :character_id, :characters
+
       id :skill_id, null: false
       id :training_id, null: false
+      Integer :base
     end
   end
 
