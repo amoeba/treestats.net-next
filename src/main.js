f613af9baa8029f87bdf60e561f18f60230d32e6
Bryce Mecum <petridish@gmail.com>
2019-09-02 18:32:06 -0800

    Rough in tabs

--- src/main.js
+++ src/main.js
@@ -1,8 +1,31 @@
 import chain from "./chain.js";
+import { select_by_class, has_class, activate } from "./tabs.js";
 
 // Chain
 const chainEl = document.getElementById("chain");
 
 if (chainEl) {
   chain(chainEl, chainEl.dataset.server, chainEl.dataset.character);
+}
+
+// Tabs
+var tabcontainers = select_by_class("tabcontainer");
+
+for(var i = 0; i < tabcontainers.length; i++) {
+  var children = tabcontainers[i].childNodes;
+
+  for(var j = 0; j < children.length; j++) {
+    if(has_class(children[j], "tabbar")) {
+      var tabs = children[j].childNodes;
+
+      for(var k = 0; k < tabs.length; k++) {
+        if(has_class(tabs[k], "tab")) {
+          tabs[k].addEventListener("click", function() {
+            activate(this);
+          });
+
+        }
+      }
+    }
+  }
 }
