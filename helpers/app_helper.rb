fede6a4cf822c7cbc2748de3087ea985ebc7e1fa
Bryce Mecum <petridish@gmail.com>
2019-09-12 11:23:18 -0800

    Freeze some string literals

--- helpers/app_helper.rb
+++ helpers/app_helper.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Sinatra
   module AppHelper
     def get_page(params)
--- helpers/rankings_helper.rb
+++ helpers/rankings_helper.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 require_relative "./enum_helper"
 
 module Sinatra
