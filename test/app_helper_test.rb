3b548d7abadd171f7f2b667036aef6ffaec92157
Bryce Mecum <petridish@gmail.com>
2019-10-06 01:16:50 -0800

    Fix up app_helper test

--- test/app_helper_test.rb
+++ test/app_helper_test.rb
@@ -4,18 +4,18 @@ class AppHelperTest < MiniTest::Test
   include Sinatra::AppHelper
 
   def test_get_page
-    assert get_page(nil) == 1
-    assert get_page("hi!") == 1
-    assert get_page({page: -1}) == 1
-    assert get_page({page: "asdf"}) == 1
-    assert get_page({page: 2}) == 2
+    assert_equal(get_page(nil), 1)
+    assert_equal(get_page("hi!"), 1)
+    assert_equal(get_page({page: -1}), 1)
+    assert_equal(get_page({page: "asdf"}), 1)
+    assert_equal(get_page({page: 2}), 2)
   end
 
   def test_to_params
-    assert to_params(nil) == ""
-    assert to_params([]) == ""
-    assert to_params({a: 1}) == "?a=1"
-    assert to_params({a: 1, b: "foo"}) == "?a=1&b=foo"
-    assert to_params({a: 1, b: "foo bar"}) == "?a=1&b=foo%20bar"
+    assert_equal(to_params(nil), "")
+    assert_equal(to_params([]), "")
+    assert_equal(to_params({a: 1}), "a=1")
+    assert_equal(to_params({a: 1, b: "foo"}), "a=1&b=foo")
+    assert_equal(to_params({a: 1, b: "foo bar"}), "a=1&b=foo+bar")
   end
 end
